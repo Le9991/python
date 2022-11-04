6f060db29ede5abb18f77aecb17cf49c14e8dcc8
Le9991 <117416202+Le9991@users.noreply.github.com>
2022-11-03 22:03:24 -0300

    Add files via upload

---
+++ exer01.py
@@ -0,0 +1,4 @@
+#caracter de comentario 
+#print para imprimir informações na tela
+print('\nOlá Mundo Python')
+print('\nMeu nome é: ', 'Leandra', ',', 23,'anos')
---
+++ exer02.py
@@ -0,0 +1,3 @@
+#usando input para a entrada de dados
+print('Seja Bem-vindo a aula de ' + 'Python')
+print('Olá {}' .format(input('Qual o seu nome?')))

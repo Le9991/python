c90b87ad249197190d1b8019ddbf730641c658cd
Le9991 <117416202+Le9991@users.noreply.github.com>
2022-11-03 22:22:44 -0300

    Add files via upload

---
+++ exer03.py
@@ -0,0 +1,14 @@
+#trabalhando com variaveis
+#num1 = 10
+#num2 = 5
+num1 = input('informe o primeiro valor:')
+num2 = input('informe o segundo valor:')
+
+print(type(num1))
+
+soma = num1 + num2
+print('A Soma é: ', soma)
+
+soma1 = int(num1) + int(num2)
+print('A soma correta é: {}' . format(soma1))
+
